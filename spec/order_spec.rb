8fffd8a862e8d7292f0842a225a99dd591986198
alwinruby <alwinsolanky@gmail.com>
2017-06-03 23:14:50 +0100

    refactored order test

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -2,4 +2,19 @@ require 'order'
 
 describe Order do
 
+  subject(:order) { described_class.new }
+
+  let(:dishes) do
+    {
+      shish: 2,
+      donner: 3
+    }
+  end
+
+  it 'selects a number of dishes off the menu' do
+    order.add(:shish, 2)
+    order.add(:donner, 3)
+    expect(order.dishes).to eq(dishes)
+  end
+
 end
