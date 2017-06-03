ababb8595c9fa90fd101765333f61a191ce1575b
alwinruby <alwinsolanky@gmail.com>
2017-06-03 20:20:20 +0100

    refactored first test

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,10 +1,14 @@
 require 'takeaway'
 
 describe Takeaway do
-  subject(:takeaway) { described_class.new }
-  
+
+  subject(:takeaway) { described_class.new(menu: menu) }
+
+  let(:menu) { double(:menu, print: printed_menu) }
+  let(:printed_menu) { 'Kebab £5.50' }
+
   it 'shows menu with availible dishes and their corresponding prices' do
-    expect(takeaway.print_menu).to eq('Menu List')
+    expect(takeaway.print_menu).to eq(printed_menu)
   end
 
 end
